4ba5561633d7e60fcf82542436c528ebf652d364
SMCART17 <smcart17@caledonian.ac.uk>
2014-09-25 16:29:07 +0100

    Initial project started

---
+++ GP2LabsSmcart17/main.cpp
@@ -0,0 +1,35 @@
+//Header files
+#include <iostream>
+
+#include <SDL.h>
+
+//Pointer to our SDL Windows
+SDL_Window *window;
+
+//Constants to control window creation
+const int WINDOW_WIDTH = 640;
+
+
+
+
+
+
+
+
+//Global functions
+void InitWindow(int width, int height, bool fullscreen)
+{
+
+}
+
+void CleanUp()
+{
+
+}
+
+//Main Method - Entry Point
+int main(int argc, char* arg[])
+{
+	return 0;
+}
+
